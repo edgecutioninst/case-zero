bd7808c64e9b3756877a4a145c639c37110f614a
edgecutionist <harshkr4525@gmail.com>
2026-07-08 20:39:48 +0530

    ADD: vercel origin

--- server/app/main.py
+++ server/app/main.py
@@ -29,7 +29,7 @@ INITIAL_TRANSMISSION = {
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://case-zero-liard.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
