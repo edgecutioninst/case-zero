1490cfd4606b45b9deec9d91b4f74f60e61c713c
edgecutionist <harshkr4525@gmail.com>
2026-07-08 20:15:50 +0530

    PATCH: url change in client folder

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -71,7 +71,10 @@ export default function GameDashboard() {
 
     const loadSaveFile = async () => {
       try {
-        const res = await fetch(`http://localhost:8000/api/state/${userEmail}`);
+        
+        const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+        const res = await fetch(`${API_URL}/api/state/${userEmail}`);
+
         const data = await res.json();
         
         if (data.status === "success") {
