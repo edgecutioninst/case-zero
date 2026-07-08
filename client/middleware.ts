0fa7c6efa2c054de73d98b79a5442298fe34aca9
edgecutionist <harshkr4525@gmail.com>
2026-07-08 21:01:28 +0530

    PATCH: middleware.ts fix

--- client/middleware.ts
+++ client/middleware.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
 
+export const runtime = "nodejs";
+
 export async function middleware(req: NextRequest) {
   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = req.nextUrl;
